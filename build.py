b5c59a38f0b376256ffa036031999ad4ab4ae368
newlawrence <alorenzo.md@gmail.com>
2017-10-18 22:17:44 +0200

    Added build script

---
+++ build.py
@@ -0,0 +1,48 @@
+import re
+import os
+import cffi
+
+
+with open('include/calculate.h', 'r') as handler:
+    header = handler.read()
+
+header = re.sub(r'#ifdef __cplusplus(.|\n)*?(#else\n|#endif\n)', '', header)
+header = re.sub(r'#.*\n', '', header)
+header = re.sub(r'.*\\\n', '', header)
+header = re.sub(r' +\)\(.*\)\n', '', header)
+header = re.sub(r'\n\)\n', '\n', header)
+header = re.sub(r'\n\)\n', '\n', header)
+header = '\n'.join([
+    re.sub(r'^((?:calculate|void|size_t|int|double).+)$', r'extern \1', line)
+    for line in header.split('\n')
+])
+
+header += r'''
+extern "Python+C" double _calculate_callback1(void*, double);
+extern "Python+C" double _calculate_callback2(void*, double, double);
+extern "Python+C" double _calculate_callback3(void*, double, double, double);
+'''
+
+ffi = cffi.FFI()
+ffi.cdef(header)
+ffi.set_source(
+    'calculate._calculate', r'#include "calculate.h"',
+    sources=['source/binding.cpp'],
+    libraries=['c++'],
+    include_dirs=['include'],
+    extra_compile_args=[
+        '-std=c++14',
+        '-pedantic',
+        '-stdlib=libc++',
+        '-Wall',
+        '-Werror'
+    ],
+    source_extension='.cpp'
+)
+
+
+if __name__ == '__main__':
+    ffi.compile(verbose=True)
+    os.remove('calculate/_calculate.cpp')
+    os.remove('calculate/_calculate.o')
+    os.remove('source/binding.o')

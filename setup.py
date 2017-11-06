8ed4c4b8be352e9f84d86aa2a078698d24b267be
alorenzo.md@gmail.com <alorenzo.md@gmail.com>
2017-11-06 16:27:30 +0100

    Lower CFFI version requirement

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
         version='2.0.0rc1',
         packages=['calculate'],
         package_dir={'calculate': './calculate'},
-        setup_requires=['cffi>=1.11.2'],
-        install_requires=['cffi>=1.11.2'],
+        setup_requires=['cffi>=1.10.0'],
+        install_requires=['cffi>=1.10.0'],
         cffi_modules=['build.py:ffi']
     )
